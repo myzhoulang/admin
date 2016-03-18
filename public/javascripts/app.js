f9b7afb2f1b64ef5ecb44987f5aa3a2b492d4296
unknown <>
2016-03-19 00:26:46 +0800

    add buttons

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,42 +2,42 @@
  * 创建app 模块 并在这里配置路由 最后返回这个模块
  */
 
-define(['angular', '$'], function(angular, $){
+define(['angular', '$'], function (angular, $) {
   var app = angular.module('app', ['ngRoute', 'ui.router']);
   app.run([
-    '$rootScope', 
-    '$state', 
-    '$stateParams', 
-    function( $rootScope, $state, $stateParams) {
+    '$rootScope',
+    '$state',
+    '$stateParams',
+    function ($rootScope, $state, $stateParams) {
       $rootScope.$state = $state;
       $rootScope.$stateParams = $stateParams;
-  }]);
+    }]);
 
   // 将 controllerProvider 挂载到app 上
-  app.config(['$controllerProvider', function($controllerProvider) {
+  app.config(['$controllerProvider', function ($controllerProvider) {
     app.registerController = $controllerProvider.register;
   }]);
-  
+
   // 配置路由
   app.config([
-    '$httpProvider', 
-    '$locationProvider', 
+    '$httpProvider',
+    '$locationProvider',
     '$urlRouterProvider',
     '$stateProvider',
-    function($httpProvider, $locationProvider, $urlRouterProvider, $stateProvider){
+    function ($httpProvider, $locationProvider, $urlRouterProvider, $stateProvider) {
       $locationProvider.html5Mode(true);
       $urlRouterProvider.when("/", "/login");
-      
+
       // 登录页面
       $stateProvider
         .state('login', {
-          url:'/login',
-          templateUrl:'/router/login/login.tpl.html',
+          url: '/login',
+          templateUrl: '/router/login/login.tpl.html',
           controller: 'loginCtrl',
           resolve: {
-            loadCtrl: ['$q',function ($q){
+            loadCtrl: ['$q', function ($q) {
               var defer = $q.defer();
-              require(["loginCtrl"], function() {
+              require(["loginCtrl"], function () {
                 defer.resolve()
               });
               return defer.promise;
@@ -46,64 +46,72 @@ define(['angular', '$'], function(angular, $){
         })
         .state('home', {
           abstract: true,
-          templateUrl:'/router/home/home.tpl.html',
+          templateUrl: '/router/home/home.tpl.html',
           controller: 'homeCtrl',
           resolve: {
-              loadCtrl: ['$q',function ($q){
-                var defer = $q.defer();
-                require(["homeCtrl"], function() {
-                  defer.resolve()
-                });
-                return defer.promise;
-              }]
-            }
-        })
-      .state('home.index', {
-        url:'/index',
-        templateUrl:'/router/home/index.tpl.html',
-        controller: 'indexCtrl',
-        resolve: {
-            loadCtrl: ['$q',function ($q){
+            loadCtrl: ['$q', function ($q) {
               var defer = $q.defer();
-              require(["indexCtrl"], function() {
+              require(["homeCtrl"], function () {
                 defer.resolve()
               });
               return defer.promise;
             }]
           }
-      })
-      .state('home.metrics', {
-        url:'/metrics',
-        templateUrl:'/router/metrics/metrics.tpl.html',
-        controller:'metricsCtrl',
-        resolve: {
-          loadCtrl:['$q', function($q){
-            var defer = $q.defer();
-            require(["metricsCtrl"], function(){
-              defer.resolve()
-            });
-            return defer.promise;
-          }]
-        }
-      })
-      .state('home.widgets', {
-        url:'/widgets',
-        templateUrl:'/router/widgets/widgets.tpl.html',
-        controller:'widgetsCtrl',
-        resolve: {
-          loadCtrl:['$q', function($q){
-            var defer = $q.defer();
-            require(["widgetsCtrl"], function(){
-              defer.resolve()
-            });
-            return defer.promise;
-          }]
-        }
-      })
-  }]);
-  
-  $(document).ready(function() {
-	  angular.bootstrap(document, ['app']);
-	});
+        })
+        .state('home.index', {
+          url: '/index',
+          templateUrl: '/router/home/index.tpl.html',
+          controller: 'indexCtrl',
+          resolve: {
+            loadCtrl: ['$q', function ($q) {
+              var defer = $q.defer();
+              require(["indexCtrl"], function () {
+                defer.resolve()
+              });
+              return defer.promise;
+            }]
+          }
+        })
+        .state('home.metrics', {
+          url: '/metrics',
+          templateUrl: '/router/metrics/metrics.tpl.html',
+          controller: 'metricsCtrl',
+          resolve: {
+            loadCtrl: ['$q', function ($q) {
+              var defer = $q.defer();
+              require(["metricsCtrl"], function () {
+                defer.resolve()
+              });
+              return defer.promise;
+            }]
+          }
+        })
+        .state('home.widgets', {
+          url: '/widgets',
+          templateUrl: '/router/widgets/widgets.tpl.html',
+          controller: 'widgetsCtrl',
+          resolve: {
+            loadCtrl: ['$q', function ($q) {
+              var defer = $q.defer();
+              require(["widgetsCtrl"], function () {
+                defer.resolve()
+              });
+              return defer.promise;
+            }]
+          }
+        })
+        .state('home.ui', {
+          abstract: true,
+          template:'<div ui-view=""></div>'
+        })
+        .state('home.ui.buttons', {
+          url: '/uiButtons',
+          templateUrl: '/router/ui/buttons.tpl.html'
+        })
+    }]);
+
+  $(document).ready(function () {
+    angular.bootstrap(document, ['app']);
+  });
   return app;
 });
